c99d4963ca5b80ef7c100294d4664edc0389b777
RuizMiguel-A <ruizmigueldev@gmail.com>
2025-07-31 12:17:24 -0500

    front-back beta 0.1

--- encuesta_beta/settings.py
+++ encuesta_beta/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(vobrtl!-q&1x23%^iac&j844f2hqfy9!o-226lia%=#htsm2&'
+SECRET_KEY = 'django-insecure-(vobrtl!-q    &1x23%^iac&j844f2hqfy9!o-226lia%=#htsm2&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -117,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'encuestas', 'static'),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
